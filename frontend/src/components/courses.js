3fccd34e3e520ba65259495db3cc743182f22e6f
Jeremy Wizenfeld <jeremy.wizenfeld@gmail.com>
2022-01-19 20:29:05 -0800

    Frontend Assignment Changes

--- frontend/src/components/courses.js
+++ frontend/src/components/courses.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   StyleSheet,
@@ -11,6 +11,7 @@ import {
   SafeAreaView,
   Image,
   Modal,
+  Pressable,
 } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -19,7 +20,11 @@ import {
 } from "../redux/features/user/userSlice";
 import { selectCourses } from "../redux/features/course/courseSlice";
 import { ListItem } from "react-native-elements";
-import { selectAssignments } from "../redux/features/assignment/assignmentSlice";
+import {
+  selectAssignments,
+  getCourseSpecificAssignments,
+  selectCourseSpecficAssignments,
+} from "../redux/features/assignment/assignmentSlice";
 import courseScreenStyles from "../styles/courseScreenStyles";
 import { getCourses } from "./getCourses";
 import { getAssignments } from "./getAssignments";
@@ -30,21 +35,34 @@ export default function Courses() {
   const courses = useSelector(selectCourses); // Gets array of objects for all user courses
   const assignments = useSelector(selectAssignments); // Gets array of objects for all user assignments
   const isSchoologyAuthenticated = useSelector(selectIsSchoologyAuthenticated); // Gets boolean of whether their Schoology account is authenticated or not
+  const courseSpecficAssignments = useSelector(selectCourseSpecficAssignments);
   const [isRefreshing, setIsRefreshing] = useState(false); // Used for pulldown refresh
   const [modalVisible, setModalVisible] = useState(false);
-  const [modalCourseName, setModalCourseName] = useState("");
+  const [modalData, setModalData] = useState({});
   const getAllCourses = getCourses(dispatch);
   const getAllAssignments = getAssignments(dispatch);
+  const [courseData, setCourseData] = useState({});
 
-  const handleOnCoursePress = (item) => {
+  const handleOnCoursePressIn = (item) => {
+    setCourseData({ token: token, id: item.id });
     setModalVisible(true);
-    setModalCourseName(item);
+    setModalData({ name: item.name, grade: item.grade });
+  };
+
+  const getAssignmentsForCourse = (assignments) => {
+    return assignments.map((item, index) => (
+      <Text key={index}>{item.name}</Text>
+    ));
   };
 
   // Retrieves all courses any time the tab renders or user signs in with Schoology
   useEffect(() => {
     getAllCourses(token, isSchoologyAuthenticated);
   }, [isSchoologyAuthenticated]);
+
+  useEffect(() => {
+    dispatch(getCourseSpecificAssignments(courseData));
+  }, [courseData]);
 
   // Retreives all courses on pull-down refresh; this function is passed into the flatlist
   const onRefresh = () => {
@@ -66,7 +84,7 @@ export default function Courses() {
 
   return (
     <SafeAreaView style={courseScreenStyles.container}>
-      <Modal animationType="fade" visible={modalVisible} transparent={false}>
+      <Modal animationType="slide" visible={modalVisible} transparent={false}>
         <SafeAreaView>
           <TouchableOpacity
             onPress={() => {
@@ -74,34 +92,36 @@ export default function Courses() {
             }}
           >
             <Text style={courseScreenStyles.closeText}>Go back</Text>
-            <Text>{modalCourseName}</Text>
+            <Text>{modalData.name}</Text>
+            <Text>ASSIGNMENTS:</Text>
+            {getAssignmentsForCourse(courseSpecficAssignments)}
           </TouchableOpacity>
         </SafeAreaView>
       </Modal>
-      <View style={courseScreenStyles.courseBorder}>
-
-      <FlatList
-        onRefresh={onRefresh}
-        refreshing={isRefreshing}
-        ListHeaderComponent={ListHeader}
-        data={courses}
-        // missing item key - possibly uses db id instead solution would be keyExtractorgit
-        renderItem={({ item }) => {
-          return (
-            <TouchableOpacity onPress={() => handleOnCoursePress(item.name)}>
-              <View style={courseScreenStyles.courseBorder}>
-                <ListItem.Title style={courseScreenStyles.courseTitle}>
-                  {item.name}
-                </ListItem.Title>
-              </View>
-            </TouchableOpacity>
-          );
-        }}
+      <View>
+        <FlatList
+          onRefresh={onRefresh}
+          refreshing={isRefreshing}
+          ListHeaderComponent={ListHeader}
+          data={courses}
+          // missing item key - possibly uses db id instead solution would be keyExtractorgit
+          renderItem={({ item }) => {
+            return (
+              <Pressable
+                onPressIn={() => {
+                  handleOnCoursePressIn(item);
+                }}
+              >
+                <View style={courseScreenStyles.courseBorder}>
+                  <ListItem.Title style={courseScreenStyles.courseTitle}>
+                    {item.name}
+                  </ListItem.Title>
+                </View>
+              </Pressable>
+            );
+          }}
         />
-        </View>
-        <View >
-          <Text style={courseScreenStyles.test}>Text</Text>
-        </View>
+      </View>
     </SafeAreaView>
   );
 }
