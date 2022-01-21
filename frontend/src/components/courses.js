ecea018e49ccbac28f105470018a9212f0af4bd2
Jeremy Wizenfeld <jeremy.wizenfeld@gmail.com>
2022-01-21 00:16:37 -0800

    Changed Color Radio Buttons

--- frontend/src/components/courses.js
+++ frontend/src/components/courses.js
@@ -24,7 +24,6 @@ import {
 } from "../redux/features/course/courseSlice";
 import { ListItem } from "react-native-elements";
 import {
-  selectAssignments,
   getCourseSpecificAssignments,
   selectCourseSpecficAssignments,
 } from "../redux/features/assignment/assignmentSlice";
@@ -32,13 +31,16 @@ import courseScreenStyles from "../styles/courseScreenStyles";
 import { getCourses } from "./getCourses";
 import { getAssignments } from "./getAssignments";
 import { Ionicons } from "@expo/vector-icons";
-import { RadioButton } from "react-native-paper";
+import RadioForm, {
+  RadioButton,
+  RadioButtonInput,
+} from "react-native-simple-radio-button";
+import { radioButtonsData } from "./courseRadioButtons";
 
 export default function Courses() {
   const dispatch = useDispatch();
   const token = useSelector(selectToken); // Gets string of user token from DB
   const courses = useSelector(selectCourses); // Gets array of objects for all user courses
-  const assignments = useSelector(selectAssignments); // Gets array of objects for all user assignments
   const isSchoologyAuthenticated = useSelector(selectIsSchoologyAuthenticated); // Gets boolean of whether their Schoology account is authenticated or not
   const courseSpecficAssignments = useSelector(selectCourseSpecficAssignments);
   const [isRefreshing, setIsRefreshing] = useState(false); // Used for pulldown refresh
@@ -46,38 +48,57 @@ export default function Courses() {
   const [modalData, setModalData] = useState({});
   const getAllCourses = getCourses(dispatch);
   const getAllAssignments = getAssignments(dispatch);
-  const [courseData, setCourseData] = useState({});
-  const [checkedColor, setCheckedColor] = useState("first");
-  const [prefrencesData, setPrefrencesData] = useState({});
+  const [radioButtons, setRadioButtons] = useState(radioButtonsData);
 
   const handleOnCoursePressIn = (item) => {
-    setCourseData({ token: token, id: item.id });
-    setModalVisible(true);
     setModalData({
+      id: item.id,
+      token: token,
       name: item.name,
       grade: item.grade,
       color: item.color,
       priority: item.priority,
       notifications: item.notifications,
     });
-    setPrefrencesData({
-      id: item.id,
-      token: token,
-      color: item.color,
-      priority: item.priority,
-      notifications: item.notifications,
-    });
+    setModalVisible(true);
   };
 
   const handleAssignmentListEmpty = () => {
-    return <Text style={courseScreenStyles.courseTitle}>No assignments</Text>;
+    return <Text style={courseScreenStyles.courseTitle}>No Assignments</Text>;
+  };
+
+  const handleRadioButtonColor = () => {
+    if (modalData.color === "red") {
+      return 0;
+    }
+    if (modalData.color === "orange") {
+      return 1;
+    }
+    if (modalData.color === "yellow") {
+      return 2;
+    }
+    if (modalData.color === "green") {
+      return 3;
+    }
+    if (modalData.color === "blue") {
+      return 4;
+    }
+    if (modalData.color === "purple") {
+      return 5;
+    }
   };
 
   const NotificationCheckBox = () => {
-    const [courseNotifications, setCourseNotifications] = useState(true);
+    const [courseNotifications, setCourseNotifications] = useState(
+      modalData.notifications
+    );
 
     function onCheckmarkPress() {
       setCourseNotifications(!courseNotifications);
+      setModalData((prevState) => ({
+        ...prevState,
+        notifications: !courseNotifications,
+      }));
     }
 
     return (
@@ -95,14 +116,54 @@ export default function Courses() {
     );
   };
 
+  const CourseColorRadioButtons = () => {
+    const [checkedColor, setCheckedColor] = useState(modalData.color);
+    const [value, setValue] = useState(handleRadioButtonColor());
+
+    function onRadioPress(checkedColor, i) {
+      setCheckedColor(checkedColor);
+      setModalData((prevState) => ({
+        ...prevState,
+        color: checkedColor,
+      }));
+      setValue(i);
+    }
+
+    return (
+      <View style={courseScreenStyles.radioButton}>
+        <RadioForm formHorizontal={true} animation={true} initial={value}>
+          {/* To create radio buttons, loop through your array of options */}
+          {radioButtons.map((obj, i) => (
+            <RadioButton labelHorizontal={true} key={i}>
+              {/*  You can set RadioButtonLabel before RadioButtonInput */}
+              <RadioButtonInput
+                obj={obj}
+                index={i}
+                isSelected={value === i}
+                onPress={() => onRadioPress(obj.value, i)}
+                borderWidth={1}
+                buttonInnerColor={value === i ? checkedColor : "#000"}
+                buttonOuterColor={value === i ? "#2196f3" : "#000"}
+                buttonSize={30}
+                buttonOuterSize={40}
+                buttonStyle={{}}
+                buttonWrapStyle={{ marginLeft: 20 }}
+              />
+            </RadioButton>
+          ))}
+        </RadioForm>
+      </View>
+    );
+  };
+
   // Retrieves all courses any time the tab renders or user signs in with Schoology
   useEffect(() => {
     getAllCourses(token, isSchoologyAuthenticated);
   }, [isSchoologyAuthenticated]);
 
   useEffect(() => {
-    dispatch(getCourseSpecificAssignments(courseData));
-  }, [courseData]);
+    dispatch(getCourseSpecificAssignments(modalData));
+  }, [modalData]);
 
   // Retreives all courses on pull-down refresh; this function is passed into the flatlist
   const onRefresh = () => {
@@ -122,6 +183,42 @@ export default function Courses() {
     );
   };
 
+  const handleModalNameColor = () => {
+    if (modalData.color) {
+      return `${modalData.color}`;
+    } else {
+      return "black";
+    }
+  };
+
+  const styles = (color) =>
+    StyleSheet.create({
+      courseView: {
+        borderColor: color,
+        borderWidth: 3,
+        borderRadius: 30,
+        width: "100%",
+        height: 45,
+        marginBottom: 12,
+        marginTop: 12,
+      },
+    });
+
+  const styles2 = StyleSheet.create({
+    courseModalName: {
+      fontSize: 30,
+      textAlign: "center",
+      justifyContent: "center",
+      color: handleModalNameColor(),
+    },
+    courseTitle: {
+      fontSize: 24,
+      color: "black",
+      textAlign: "center",
+      justifyContent: "center",
+    },
+  });
+
   return (
     <SafeAreaView style={courseScreenStyles.container}>
       <Modal
@@ -129,7 +226,8 @@ export default function Courses() {
         visible={modalVisible}
         transparent={false}
         onDismiss={() => {
-          dispatch(updateUserCoursePrefrences(prefrencesData));
+          dispatch(updateUserCoursePrefrences(modalData));
+          getAllCourses(token, isSchoologyAuthenticated);
         }}
       >
         <SafeAreaView>
@@ -140,16 +238,14 @@ export default function Courses() {
           >
             <Text style={courseScreenStyles.closeText}>Return to Courses</Text>
           </TouchableOpacity>
-          <Text style={courseScreenStyles.courseModalName}>
-            {modalData.name}
-          </Text>
+          <Text style={styles2.courseModalName}>{modalData.name}</Text>
 
           <View>
             <View style={courseScreenStyles.assignmentBorder}>
               <FlatList
                 ListEmptyComponent={handleAssignmentListEmpty}
                 data={courseSpecficAssignments}
-                // missing item key - possibly uses db id instead solution would be keyExtractorgit
+                // Missing item key - possibly use course id instead with keyExtractor
                 renderItem={({ item }) => {
                   return (
                     <View style={courseScreenStyles.courseBorder}>
@@ -162,18 +258,11 @@ export default function Courses() {
               />
             </View>
           </View>
-
+          <Text style={courseScreenStyles.coursePreferences}>Prefrences</Text>
           <Text>Color: {modalData.color}</Text>
-          
-        <RadioButton.Group onValueChange={checkedColor => setCheckedColor(checkedColor)} checkedColor={checkedColor}>
-         <RadioButton.Item label="green" value="first" />
-        <RadioButton.Item label="red" value="second" />
-        <RadioButton.Item label="purple" value="third" />
-        </RadioButton.Group>
 
-         
-
-          <Text>{modalData.priority}</Text>
+          <CourseColorRadioButtons />
+          <Text>Priority: {modalData.priority}</Text>
           <NotificationCheckBox />
           {modalData.notifications ? (
             <Text>Notifications Enabled</Text>
@@ -196,8 +285,8 @@ export default function Courses() {
                   handleOnCoursePressIn(item);
                 }}
               >
-                <View style={courseScreenStyles.courseBorder}>
-                  <ListItem.Title style={courseScreenStyles.courseTitle}>
+                <View style={styles(item.color).courseView}>
+                  <ListItem.Title style={styles2.courseTitle}>
                     {item.name}
                   </ListItem.Title>
                 </View>
