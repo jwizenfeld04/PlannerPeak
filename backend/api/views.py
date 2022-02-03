71d043d2d8f05c0a57a98605292a4917f2b4c55b
Jeremy Wizenfeld <jeremy.wizenfeld@gmail.com>
2022-02-02 23:44:16 -0800

    Schedule Test

--- backend/api/views.py
+++ backend/api/views.py
@@ -388,9 +388,11 @@ class ScheduleAssignments(APIView):
 def addScheduleTimes(assignments):
     current_time = datetime.now()
     for assignment in assignments:
-        assignment.scheduled_start = current_time
-        assignment.scheduled_finish = current_time + timedelta(minutes=1)
-        assignment.save(
+        current_assignment = Assignment.objects.get(id=assignment.id)
+        current_assignment.scheduled_start = current_time
+        current_assignment.scheduled_finish = current_time + \
+            timedelta(minutes=1)
+        current_assignment.save(
             update_fields=['scheduled_start', 'scheduled_finish'])
         current_time += timedelta(minutes=1)
 
