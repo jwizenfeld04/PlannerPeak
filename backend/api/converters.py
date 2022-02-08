fcdbd9db42881e57adbabc932d75d4e0a4dcaef5
Jeremy Wizenfeld <jeremy.wizenfeld@gmail.com>
2022-02-08 12:14:51 -0800

    comments

--- backend/api/converters.py
+++ backend/api/converters.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 class DateConverter:
     regex = '\d{4}-\d{1,2}-\d{1,2}'
-    format = '%Y-%m-%d'  # 2022-02-07
+    format = '%Y-%m-%d'  # Format Example: 2022-02-07
 
     def to_python(self, value):
         return datetime.strptime(value, self.format).date()
