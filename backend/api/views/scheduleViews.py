49f15beffee0b066276c218f86a4861cf8836a11
Jeremy Wizenfeld <jeremy.wizenfeld@gmail.com>
2022-02-08 14:38:21 -0800

    Fixed assignment schedule  api requests

--- backend/api/views/scheduleViews.py
+++ backend/api/views/scheduleViews.py
@@ -19,16 +19,41 @@ class ScheduleAssignments(APIView):
         return Response({'Success': "Added Assignment Times"}, status=HTTP_200_OK)
 
 
+class CurrentSchedule(APIView):
+    serializer_class = AssignmentSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        assignments = getAssignmentsCurrentSchedule(request.user.id)
+        if len(assignments) == 0:
+            return Response({"No Assignments": "Empty Schedule"}, status=HTTP_204_NO_CONTENT)
+        ordered_assignments = orderAssignments(assignments)
+        return Response(self.serializer_class(ordered_assignments, many=True).data, status=HTTP_200_OK)
+
+
+class SpecficDateSchedule(APIView):
+    serializer_class = AssignmentSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None, *args, **kwargs):
+        assignments = getAssignmentsScheduleDate(
+            request.user.id, self.kwargs['date'])
+        if len(assignments) == 0:
+            return Response({"No Assignments": "Empty Schedule"}, status=HTTP_204_NO_CONTENT)
+        ordered_assignments = orderAssignments(assignments)
+        return Response(self.serializer_class(ordered_assignments, many=True).data, status=HTTP_200_OK)
+
+
 def addScheduleTimes(assignments):
     current_time = datetime.now(tz=timezone.utc)
     for assignment in assignments:
         current_assignment = Assignment.objects.get(id=assignment.id)
-        scheduled_start = current_time + timedelta(minutes=1)
-        current_assignment.scheduled_start = scheduled_start.strftime(
-            '%Y-%-m-%-d %-H:%M:%S')
-        scheduled_finish = current_time + timedelta(minutes=3)
-        current_assignment.scheduled_finish = scheduled_finish.strftime(
-            '%Y-%-m-%-d %-H:%M:%S')
+        current_assignment.scheduled_start = current_time + \
+            timedelta(minutes=1)
+        current_assignment.scheduled_finish = current_time + \
+            timedelta(minutes=3)
         current_assignment.save(
             update_fields=['scheduled_start', 'scheduled_finish'])
         current_time += timedelta(minutes=2)
@@ -51,23 +76,6 @@ def getAssignments(user_id):
     return flatlist_all_assignments
 
 
-def getAssignmentsForSchedule(user_id):
-    course_ids = Course.objects.filter(
-        user_id=user_id).values_list('pk', flat=True)
-    if len(course_ids) == 0:
-        return Response({"No Content": "No Assignments Found"}, status=HTTP_204_NO_CONTENT)
-    all_assignments = []
-    for id in course_ids:
-        assignments = Assignment.objects.filter(
-            course_id=id).order_by('scheduled_start')
-        if len(assignments) > 0:
-            all_assignments.append(
-                assignments)
-    flatlist_all_assignments = [
-        element for sublist in all_assignments for element in sublist]
-    return flatlist_all_assignments
-
-
 def orderAssignments(assignments):
     for i in range(len(assignments)):
         # Find the minimum element in remaining
@@ -82,21 +90,23 @@ def orderAssignments(assignments):
     return assignments
 
 
-class CurrentSchedule(APIView):
-    serializer_class = AssignmentSerializer
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, format=None):
-        assignments = getAssignmentsForSchedule(request.user.id)
-        if len(assignments) == 0:
-            return Response({"No Content": "No Assignments Found"}, status=HTTP_204_NO_CONTENT)
-        current_time = datetime.now(timezone.utc)
-        # Removes all assignments from array that passed the current time
-        new_assignments = [
-            x for x in assignments if not x.scheduled_finish < current_time]
-        ordered_assignments = orderAssignments(new_assignments)
-        return Response(self.serializer_class(ordered_assignments, many=True).data, status=HTTP_200_OK)
+def getAssignmentsCurrentSchedule(user_id):
+    course_ids = Course.objects.filter(
+        user_id=user_id).values_list('pk', flat=True)
+    if len(course_ids) == 0:
+        return Response({"No Content": "No Assignments Found"}, status=HTTP_204_NO_CONTENT)
+    all_assignments = []
+    current_time = datetime.now(timezone.utc)
+    end_of_day = datetime.combine(date.today(), time.max)
+    for id in course_ids:
+        assignments = Assignment.objects.filter(
+            course_id=id, scheduled_start__range=(current_time, end_of_day)).order_by('scheduled_start')
+        if len(assignments) > 0:
+            all_assignments.append(
+                assignments)
+    flatlist_all_assignments = [
+        element for sublist in all_assignments for element in sublist]
+    return flatlist_all_assignments
 
 
 def getAssignmentsScheduleDate(user_id, date):
@@ -116,14 +126,3 @@ def getAssignmentsScheduleDate(user_id, date):
     flatlist_all_assignments = [
         element for sublist in all_assignments for element in sublist]
     return flatlist_all_assignments
-
-
-class SpecficDateSchedule(APIView):
-    serializer_class = AssignmentSerializer
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, format=None, *args, **kwargs):
-        assignments = getAssignmentsScheduleDate(
-            request.user.id, self.kwargs['date'])
-        return Response(self.serializer_class(assignments, many=True).data, status=HTTP_200_OK)
