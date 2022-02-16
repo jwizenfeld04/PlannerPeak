e40ac58f32e20c510bb2161d78abb4cda108dd70
jwizenfeld04 <81655880+jwizenfeld04@users.noreply.github.com>
2022-02-15 23:36:12 -0800

    Added scroll calendar packages (#13)
    Co-authored-by: coby karben <icecoby@gmail.com>

--- frontend/src/components/scrollCalendar.js
+++ frontend/src/components/scrollCalendar.js
@@ -4,26 +4,36 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
+  SafeAreaView,
+  FlatList,
+  StatusBar,
   Button,
   Linking,
 } from "react-native";
+import CalendarStrip from "react-native-calendar-strip";
 import { useSelector, useDispatch } from "react-redux";
 
 export default function ScrollCalendar() {
   return (
-    <View>
-        <Text>Scroll Calendar</Text>
-    </View>
+    <SafeAreaView style={{ minHeight: 1, minWidth: 1 }}>
+      <CalendarStrip
+        scrollable
+        style={{ height: 200, paddingTop: 20, paddingBottom: 10 }}
+        calendarColor={"#3343CE"}
+        calendarHeaderStyle={{ color: "white" }}
+        dateNumberStyle={{ color: "white" }}
+        dateNameStyle={{ color: "white" }}
+        iconContainer={{ flex: 0.1 }}
+      />
+    </SafeAreaView>
   );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
