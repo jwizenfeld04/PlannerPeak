b5b030ce311e58633592fb6da8c5a7d920e93323
coby karben <icecoby@gmail.com>
2022-02-24 23:30:07 -0800

    Attempt #4 at Course count

--- backend/api/views/courseViews.py
+++ backend/api/views/courseViews.py
@@ -2,7 +2,7 @@ from rest_framework import authentication
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from api.serializers import CourseSerializer
-from api.models import Course
+from api.models import Course, Assignment
 from rest_framework.response import Response
 from rest_framework.status import *
 from django.db.models import Count
